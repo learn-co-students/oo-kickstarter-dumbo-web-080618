b633d2c83d3e0d24b6fea7e08ce85a83e172b352
sarahcalande <scalande@yahoo.com>
2018-08-10 00:06:11 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+
+
+class Project
+
+@@all = []
+
+attr_accessor :title, :backers
+
+def initialize(title)
+  @title = title
+  @backers = []
+end
+
+def add_backer(project)
+  @backers << project
+  project.backed_projects << self
+end
+
+
+
+end
